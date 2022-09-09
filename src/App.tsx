5a2c1eb57766cc574ad26466013b4c5a1aecddcb
amnesia322 <dev.amnesia322@gmail.com>
2022-09-09 22:37:33 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React, {useState} from 'react';
 import './App.css';
 import Counter from "./components/Counter";
+import CounterButton from "./components/CounterButton";
 
 
 function App() {
     const [count, setCount] = useState(0)
+    const [max, setMax] = useState(false)
 
-    const addValue = () => setCount(count + 1)
+    const addValue = () => count < 5 ? setCount(count + 1) : setMax(true)
 
-    const resetValue = () => setCount(0)
+    const resetValue = () => {
+        setCount(0)
+        setMax(false)
+    }
 
 
     return (
         <div className="App">
-            <Counter countValue={count} addValue={addValue} resetValue={resetValue}/>
+            <div className='counterContainer'>
+                <Counter countValue={count} max={max}/>
+                <div className='buttonContainer'>
+                    <CounterButton name={'INC'} callBack={addValue} disabled={max}/>
+                    <CounterButton name={'RESET'} callBack={resetValue} disabled={count === 0}/>
+                </div>
+            </div>
+
         </div>
     );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,29 +1,16 @@
-import React, {useState} from 'react';
-import CounterButton from "./CounterButton";
+import React from 'react';
 
 type CounterPropsType = {
     countValue: number
-    addValue: () => void
-    resetValue: () => void
+    max: boolean
 }
 
 const Counter = (props: CounterPropsType) => {
-    const [max, setMax] = useState(false)
 
-    const onClickChangeValue = () => props.countValue < 5 ? props.addValue() : setMax(true)
-    const onClickResetValue = () => {
-        props.resetValue()
-        setMax(false)
-    }
+
 
     return (
-        <div className='counterContainer'>
-            <div className='counter'><span className={max ? 'maximum' : ''}>{props.countValue}</span></div>
-            <div className='buttonContainer'>
-                <CounterButton name={'INC'} callBack={onClickChangeValue} disabled={max}/>
-                <CounterButton name={'RESET'} callBack={onClickResetValue} disabled={props.countValue === 0}/>
-            </div>
-        </div>
+            <div className='counter'><span className={props.max ? 'maximum' : ''}>{props.countValue}</span></div>
     );
 };
 
